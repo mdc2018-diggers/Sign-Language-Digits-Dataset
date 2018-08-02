a96a6df6e3e2919f696a086047b7eabdadc1b6fb
Camila LM <camilalm@gmail.com>
2018-08-02 19:40:25 -0300

    adicionando keras em requirements

--- hello.py
+++ hello.py
@@ -1,12 +1,10 @@
+import keras
+from keras import backend as K
+from keras.models import Sequential, load_model
+from keras.preprocessing.image import ImageDataGenerator, img_to_array
 from flask import request
 from flask import jsonify
 from flask import Flask
-
-import base64
-import numpy as np
-import io
-from PIL import Image
-import keras
 
 app = Flask(__name__)
 app.debug = True
