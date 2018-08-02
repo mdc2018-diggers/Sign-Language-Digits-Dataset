b763d2866423c97e3670d58a6a443c65db0a7f09
Camila LM <camilalm@gmail.com>
2018-08-02 19:30:47 -0300

    adicionando hello para teste

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import request
+from flask import jsonify
+from flask import Flask
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def running():
+    return 'Flask is running!'
+
+if __name__ == '__main__':
+  app.run()
