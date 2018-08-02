12883239a93f98b66fb43bf7ae4a69faa584112b
Camila LM <camilalm@gmail.com>
2018-08-02 19:33:23 -0300

    Adicionando imports

--- hello.py
+++ hello.py
@@ -2,6 +2,12 @@ from flask import request
 from flask import jsonify
 from flask import Flask
 
+import base64
+import numpy as np
+import io
+from PIL import Image
+import keras
+
 app = Flask(__name__)
 app.debug = True
 
