fc50541a465cfc6bc02a37b8fc0c56849ab4201d
Camila LM <camilalm@gmail.com>
2018-08-09 23:18:53 -0300

    Melhorias em sld_app

--- sld_app.py
+++ sld_app.py
@@ -6,26 +6,36 @@ import keras
 from keras import backend as K
 from keras.models import Sequential, load_model
 from keras.preprocessing.image import ImageDataGenerator, img_to_array
+from keras.applications.mobilenet import preprocess_input
 from flask import request
 from flask import jsonify
 from flask import Flask
 import tensorflow as tf
 
+from keras.utils.generic_utils import CustomObjectScope
+from keras.layers import DepthwiseConv2D
+from keras_applications.mobilenet import relu6
+
 app = Flask(__name__)
 
 def get_model():
     global model, graph
-    model = load_model('SLD_CNN.h5')
+    with CustomObjectScope({'relu6': relu6,'DepthwiseConv2D': DepthwiseConv2D}):
+        model = load_model('SLD_CNN.h5')
     print(" ---> Model loaded!")
     graph = tf.get_default_graph()
     
 def preprocess_image(image, target_size):
-    if image.mode != "RGB":
-        image = image.convert("RGB")
+    if image.mode != "L":
+        image = image.convert("L")
     image = image.resize(target_size)
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
     return image
+
+def get_label(index):
+    correct_label = [9, 0, 7, 6, 1, 8, 4, 3, 2, 5]
+    return correct_label[index]
 
 print(" ---> Loading Keras model...")
 get_model()
@@ -37,20 +47,21 @@ def predict():
     decoded = base64.b64decode(encoded)
     with graph.as_default(): 
         image = Image.open(io.BytesIO(decoded))
-        processed_image = preprocess_image(image, target_size=(224,224))
+        processed_image = preprocess_image(image, target_size=(128, 128))
+        processed_image = preprocess_input(processed_image)
         prediction = model.predict(processed_image).tolist()
     response = {
         'prediction': {
-            '0': prediction[0][0],
-            '1': prediction[0][1],
-            '2': prediction[0][2],
-            '3': prediction[0][3],
-            '4': prediction[0][4],
-            '5': prediction[0][5],
-            '6': prediction[0][6],
-            '7': prediction[0][7],
-            '8': prediction[0][8],
-            '9': prediction[0][9]
+            '0': prediction[0][get_label(0)],
+            '1': prediction[0][get_label(1)],
+            '2': prediction[0][get_label(2)],
+            '3': prediction[0][get_label(3)],  
+            '4': prediction[0][get_label(4)],
+            '5': prediction[0][get_label(5)],
+            '6': prediction[0][get_label(6)],
+            '7': prediction[0][get_label(7)],
+            '8': prediction[0][get_label(8)],
+            '9': prediction[0][get_label(9)]
         }
     }
     return jsonify(response)
